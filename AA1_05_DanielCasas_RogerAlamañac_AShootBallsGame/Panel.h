6d5d7fd9f773cae5212edd5c80f454f3044cedbf
Daniel Casas <daniel.casas@enti.cat>
2024-04-11 13:01:14 +0200

    avance 2

--- AA1_05_DanielCasas_RogerAlamañac_AShootBallsGame/Panel.h
+++ AA1_05_DanielCasas_RogerAlamañac_AShootBallsGame/Panel.h
@@ -2,7 +2,7 @@
 #include "Player.h"
 
 int amountBalls;
-
+const int maxConsecutiveBalls = 3;
 struct Panel {
 	Ball* panel;
 	int size;
@@ -10,29 +10,36 @@ struct Panel {
 		std::cout << "ENTER THE AMOUNT OF BALLS: ";
 		std::cin >> amountBalls;
 	}
-	void insert(int position, Ball ball, Player player) {
+	void insert(int position, Ball ball) {
 		panel = new Ball[size + 1];
-		for (int i = player.position; i < amountBalls - player.position; i++) {
-			Ball aux;
-			aux = panel[i + 1];
-			panel[i + 1] = panel[i];
-
+		for (int i = size; i < ball.position; i--) {
+			panel[i - 1] = panel[i];
 		}
-		panel[player.position] = ball;
+		panel[ball.position] = ball;
 	}
 
 	int verifier(int position, Ball ball){
-		if (panel[ball.position].color == panel[ball.position + 1].color && panel[ball.position].color == panel[ball.position - 1].color) {
-			insert(position, ball);
+		if (panel[position].color == panel[position - 1].color) {
+			if (panel[position - 2].color == panel[position - 1].color) position -= 2;
+			else position -= 1;
+		}
+		else if (panel[position].color == panel[position + 1].color) {
+			if (panel[position + 1].color == panel[position + 2].color) position += 2;
+			else position += 1;
 		}
 		return position;
 	}
 
 	void deleteThree(int position){
-
+		for (int i = position; i < position + maxConsecutiveBalls; i++) {
+			panel[i].isDestroyed = true;
+		}
 	}
 
 	void insertThree(){
-		
+		int ballsToInsert = 3;
+		for(int i = size; i < ballsToInsert; i++){
+			
+		}
 	}
 };
